94adfdfa000197129fd2918b08ec1acd6e1bd939
Simran <simrantamang6008@gmail.com>
2024-09-17 12:48:19 +0530

    accept incoming changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,36 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Self-Learning',
+			title: 
+			'Self-Learning',
 			logo: {
 				src: '/src/assets/logo.png'
+			},
+			// Set English as the default language for this site.
+			defaultLocale: 'en',
+			locales: {
+				// English docs in `src/content/docs/en/`
+				en: {
+					label: 'English',
+				},
+				// Simplified Chinese docs in `src/content/docs/zh-cn/`
+				'hindi': {
+					label: 'Hindi',
+					lang: 'hindi',
+				},
 			},
 			sidebar: [
 				{ label: 'Dr. Racket', autogenerate: { directory: 'racket/' } },
 				{ label: 'Front-end', autogenerate: { directory: 'html' } },
 				{ label: 'Umang', autogenerate: { directory: 'umang' } },
 				{ label: 'CSS', autogenerate: { directory: 'css' } },
-			], 
+			],
 			components: {
 				Sidebar: './src/overrides/Sidebar.astro',
 				Pagination: './src/overrides/Pagination.astro',
 				SocialIcons: './src/components/PageCom.astro',
 			},
-			customCss : [
+			customCss: [
 				'/src/styles/custom.css'
 			]
 
